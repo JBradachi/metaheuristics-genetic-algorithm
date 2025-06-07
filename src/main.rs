dc7b931636e119b3e43e513d52d94584fe7bc555
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2025-06-07 20:55:04 -0300

    feat: tempo e 30 exec

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use genetic_algorithm::{data::Problema, genetico::genetico};
 
 fn main() {
-    let mut problema = Problema::load_from("instances/ep01.dat");
+    let mut problema = Problema::load_from("instances/ep08.dat");
 
     let tamanho_populacao: usize = 30;
 
-    let tempo_inicial = std::time::Instant::now();
     for i in 0..30 {
+        let tempo_inicial = std::time::Instant::now();
         let (solucao_inicial, melhor_solucao) = genetico(&mut problema, tamanho_populacao);
         let tempo_final = tempo_inicial.elapsed();
         println!("Iteração {i}:");
